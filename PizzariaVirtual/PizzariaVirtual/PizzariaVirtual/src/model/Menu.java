a05b520f53ca2454d8744aadda57826d7fe9a3d8
julysanguinetto <july_sanguinetto@hotmail.com>
2016-06-19 00:33:03 -0300

    Update Menu.java

--- PizzariaVirtual/PizzariaVirtual/PizzariaVirtual/src/model/Menu.java
+++ PizzariaVirtual/PizzariaVirtual/PizzariaVirtual/src/model/Menu.java
@@ -13,28 +13,51 @@ import java.util.Random;
  * @author maykaoo
  */
 public class Menu {
-    
+
     private static ArrayList<ItemMenu> menu = new ArrayList();
     private static int idPizzaDia;
 
+    /**
+     * retorna o menu da pizzaria
+     *
+     * @return menu da pizzaria
+     */
     public static ArrayList<ItemMenu> getMenu() {
         return menu;
     }
-    
-    public static void addPizza(ItemMenu item){
+
+    /**
+     * adiciona uma pizza ao menu
+     *
+     * @param item uma pizza do menu
+     */
+    public static void addPizza(ItemMenu item) {
         Menu.menu.add(item);
     }
-    
-    public static void removePizza(ItemMenu item){
+
+    /**
+     * retira uma pizza do menu
+     *
+     * @param item pizza do menu
+     */
+    public static void removePizza(ItemMenu item) {
         Menu.menu.remove(item);
     }
 
+    /**
+     * retorna o id da pizza do dia
+     *
+     * @return id da pizza do dia
+     */
     public static int getIdPizzaDia() {
         return idPizzaDia;
     }
-    
-    public static void geraPizzaDia(){
+
+    /**
+     * gera uma pizza do dia
+     */
+    public static void geraPizzaDia() {
         Random gerador = new Random();
-        Menu.idPizzaDia=gerador.nextInt(menu.size());
-    }    
+        Menu.idPizzaDia = gerador.nextInt(menu.size());
+    }
 }
