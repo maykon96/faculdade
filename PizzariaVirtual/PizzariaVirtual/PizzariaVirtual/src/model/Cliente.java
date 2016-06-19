29203e32c2f61b7912cd1057c8a02317d0c6df82
julysanguinetto <july_sanguinetto@hotmail.com>
2016-06-19 00:28:52 -0300

    Update Cliente.java

--- PizzariaVirtual/PizzariaVirtual/PizzariaVirtual/src/model/Cliente.java
+++ PizzariaVirtual/PizzariaVirtual/PizzariaVirtual/src/model/Cliente.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author maykaoo
+ * @author San
  */
 public class Cliente {
 
@@ -20,6 +20,15 @@ public class Cliente {
     private ArrayList<Cartao> cartoes = new ArrayList();
     private ArrayList<Pedido> pedidos = new ArrayList();
 
+    /**
+     * Construtor da classe
+     *
+     * @param nome nome do cliente
+     * @param endereco endereço do cliente
+     * @param telefone telefone do cliente
+     * @param cpf cpf do cliente
+     * @param cartao cartão do cliente
+     */
     public Cliente(String nome, String endereco, String telefone, String cpf, Cartao cartao) {
         this.nome = nome;
         this.endereco = endereco;
@@ -28,42 +37,92 @@ public class Cliente {
         this.cartoes.add(cartao);
     }
 
+    /**
+     * retorna o nome do cliente
+     *
+     * @return nome do cliente
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * retorna o endereço do cliente
+     *
+     * @return endereço do cliente
+     */
     public String getEndereco() {
         return endereco;
     }
 
+    /**
+     * seta o endereço do cliente
+     *
+     * @param endereco
+     */
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 
+    /**
+     * retorna o numero de telefone do cliente
+     *
+     * @return numero de telefone do cliente
+     */
     public String getNumeroTelefone() {
         return numeroTelefone;
     }
 
+    /**
+     * seta o numero de telefone do cliente
+     *
+     * @param numeroTelefone numero de telefone do cliente
+     */
     public void setNumeroTelefone(String numeroTelefone) {
         this.numeroTelefone = numeroTelefone;
     }
 
+    /**
+     * retorna o numero de cpf do cliente
+     *
+     * @return numero de cpf do cliente
+     */
     public String getCpf() {
         return cpf;
     }
 
+    /**
+     * retorna uma lista de cartões do cliente
+     *
+     * @return lista de cartões do cliente
+     */
     public ArrayList<Cartao> getCartoes() {
         return cartoes;
     }
 
+    /**
+     * adiciona um cartão no cadastro do cliente
+     *
+     * @param cartao cartão do cliente a ser adicionado
+     */
     public void addCartao(Cartao cartao) {
         this.cartoes.add(cartao);
     }
 
+    /**
+     * retorna os pedidos de pizza do cliente
+     *
+     * @return lista de pedidos do cliente
+     */
     public ArrayList<Pedido> getPedidos() {
         return pedidos;
     }
 
+    /**
+     * seta os pedidos do cliente
+     *
+     * @param pedidos pedidos do cliente
+     */
     public void setPedidos(ArrayList<Pedido> pedidos) {
         this.pedidos = pedidos;
     }
